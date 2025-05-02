030ad259b2d1cbcf028ade065db8ddfaa5872465
Eduarda Veloso <97298312+dxude@users.noreply.github.com>
2025-05-02 03:11:25 +0000

    estrutura inicial com React, rotas e pastas base

---
+++ frontend-teaxis/src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Cadastro from './pages/Cadastro';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Cadastro />} />
+    </Routes>
+  );
+}
+
+export default App;
